530bf19e6a8380cdaf69891423d57d2ffa0d2e27
Alexsatm <dzamilovaalbina2@gmail.com>
2024-11-06 16:22:45 +0300

    start project

---
+++ app.js
@@ -0,0 +1,51 @@
+// Todo----------------------------Switch Tab-----------------------------
+const tabsBtn = document.querySelectorAll('.tabsBtn')
+const tabsItem = document.querySelectorAll('.tabsItem')
+
+tabsBtn.forEach((item) => {
+    item.addEventListener('click', () => {
+        let currentBtn = item;
+        let tabId = currentBtn.getAttribute("data-tab")
+        let currentTab = document.querySelector(tabId)
+
+        if(!currentBtn.classList.contains('activeTab')) {
+            tabsBtn.forEach((item2) => {
+                item2.classList.remove('activeTab')
+            });
+
+            tabsItem.forEach((item3) => {
+                item3.classList.remove('activeTab')
+            });
+
+            currentBtn.classList.add('activeTab')
+            currentTab.classList.add('activeTab')
+        }
+    })
+})
+
+document.querySelector('.tabsBtn').click()
+
+// Todo------------------------------Portfolio btn-----------------------------
+// view to tab content on click tab buttons
+
+const tabs = document.querySelectorAll('.tab')
+const tabBtns = document.querySelectorAll('.tab2-btn')
+
+const tab_Nav = function(tabBtnClick) {
+    tabBtns.forEach((tabBtn) => {
+        tabBtn.classList.remove('active')
+    })
+
+    tabs.forEach((tab) => {
+        tab.classList.remove('active')
+    })
+
+    tabBtns[tabBtnClick].classList.add('active');
+    tabs[tabBtnClick].classList.add('active')
+}
+
+tabBtns.forEach((tabBtn, i) => {
+    tabBtn.addEventListener('click', () => {
+        tab_Nav(i);
+    })
+})
---
+++ draft.md
@@ -0,0 +1,21 @@
+## проверить на пререполение текста
+- карточки
+
+## не расширается ли контент при уменьшее страницы на большом экране
+
+## в cards вместо гридов сделать флекс
+`
+display: flex;
+flex-wrap: wrap;
+justify-content: center;
+`
+
+##  разделить логику css на модули
+
+wrapper
+background-color: #141b2b;
+
+body
+background-color: #040a1c; #0d121c
+
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus est inventore fuga! Totam neque sit vero nemo minima id magni quidem est natus quibusdam? Facilis impedit nemo accusamus sit quod.
